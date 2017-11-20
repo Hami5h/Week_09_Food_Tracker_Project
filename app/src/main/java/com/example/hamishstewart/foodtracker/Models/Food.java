23ccd091402ad56bdbee2a7cdb5708cefd30c47a
Hamish <hamishfpso@gmail.com>
2017-11-20 13:48:29 +0000

    Added setters for food model

--- app/src/main/java/com/example/hamishstewart/foodtracker/Models/Food.java
+++ app/src/main/java/com/example/hamishstewart/foodtracker/Models/Food.java
@@ -19,4 +19,24 @@ public class Food {
     public MealType getMealType() {
         return this.meal;
     }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getDate() {
+        return this.date;
+    }
+
+    public void setMealType(MealType meal) {
+        this.meal = meal;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
 }
--- app/src/test/java/com/example/hamishstewart/foodtracker/FoodTest.java
+++ app/src/test/java/com/example/hamishstewart/foodtracker/FoodTest.java
@@ -3,9 +3,11 @@ package com.example.hamishstewart.foodtracker;
 import com.example.hamishstewart.foodtracker.Models.Food;
 import com.example.hamishstewart.foodtracker.Models.MealType;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 /**
  * Created by hamishstewart on 20/11/2017.
@@ -15,9 +17,25 @@ public class FoodTest {
 
     Food food;
 
+    @Before
+    public void before() {
+        food = new Food(1, MealType.BREAKFAST, "20/11/2017");
+    }
+
     @Test
     public void canGetMealType() {
-        food = new Food(1, MealType.BREAKFAST);
         assertEquals(MealType.BREAKFAST, food.getMealType());
     }
+
+    @Test
+    public void canGetId() {
+        assertEquals(1, food.getId());
+    }
+
+    @Test
+    public void canGetDate() {
+        assertEquals("20/11/2017", food.getDate());
+    }
+
+
 }
