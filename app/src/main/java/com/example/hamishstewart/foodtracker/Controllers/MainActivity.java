f800e73a7b42d848f19548b0b5042121cd5fde78
Hamish <hamishfpso@gmail.com>
2017-11-21 14:48:29 +0000

    Android list View created

--- app/src/main/java/com/example/hamishstewart/foodtracker/Controllers/MainActivity.java
+++ app/src/main/java/com/example/hamishstewart/foodtracker/Controllers/MainActivity.java
@@ -2,14 +2,45 @@ package com.example.hamishstewart.foodtracker.Controllers;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.Toast;
 
+import com.example.hamishstewart.foodtracker.Db.FoodTrackerDatabase;
+import com.example.hamishstewart.foodtracker.Models.Food;
+import com.example.hamishstewart.foodtracker.Models.FoodList;
+import com.example.hamishstewart.foodtracker.Models.FoodListAdapter;
 import com.example.hamishstewart.foodtracker.R;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+  FoodTrackerDatabase myDb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myDb = new FoodTrackerDatabase(this);
+
+        FoodList foodList = new FoodList();
+        ArrayList<Food> food = foodList.getList();
+
+        FoodListAdapter foodAdapter = new FoodListAdapter(this, food);
+
+        ListView listView = findViewById(R.id.list);
+
+        listView.setAdapter(foodAdapter);
     }
+
+    public void getFood(View listItemSelected) {
+        Food selectedFood = (Food) listItemSelected.getTag();
+
+//        Toast.makeText(this,
+//                selectedFood.getMealType(),
+//                Toast.LENGTH_SHORT).show();
+
+    }
+
+
 }
---
+++ app/src/main/java/com/example/hamishstewart/foodtracker/Models/FoodListAdapter.java
@@ -0,0 +1,45 @@
+package com.example.hamishstewart.foodtracker.Models;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.example.hamishstewart.foodtracker.R;
+
+import java.util.ArrayList;
+
+/**
+ * Created by hamishstewart on 21/11/2017.
+ */
+
+public class FoodListAdapter extends ArrayAdapter<Food> {
+    public FoodListAdapter(Context context, ArrayList<Food> food) {
+        super(context, 0, food);
+    }
+
+    @Override
+    public View getView(int position, View listItemView, ViewGroup parent) {
+        if(listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(R.layout.food_item, parent, false);
+        }
+
+        Food currentFoodItem = getItem(position);
+
+        TextView type = listItemView.findViewById(R.id.Type);
+        type.setText(currentFoodItem.getMealType().toString());
+
+        TextView eaten = listItemView.findViewById(R.id.Eaten);
+        eaten.setText(currentFoodItem.getEaten().toString());
+
+        TextView date = listItemView.findViewById(R.id.Date);
+        date.setText((currentFoodItem.getDate().toString()));
+
+        listItemView.setTag(currentFoodItem);
+
+        return listItemView;
+    }
+
+}
