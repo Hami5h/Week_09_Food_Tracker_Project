a93e1853bb935666b7a009840c7d76a89ab172d5
Hamish <hamishfpso@gmail.com>
2017-11-23 16:37:04 +0000

    FInal app for presentation

--- app/src/main/java/com/example/hamishstewart/foodtracker/Controllers/WelcomeActivity.java
+++ app/src/main/java/com/example/hamishstewart/foodtracker/Controllers/WelcomeActivity.java
@@ -22,6 +22,7 @@ public class WelcomeActivity extends AppCompatActivity {
     public void onILoveFoddButtonClicked(View view) {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+        finish();
     }
 
 }
